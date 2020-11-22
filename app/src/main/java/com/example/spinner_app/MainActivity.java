abdb478eac982a5027999055826e997cc23469bc
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-11-22 12:11:26 +0400

    some changes

--- app/src/main/java/com/example/spinner_app/MainActivity.java
+++ app/src/main/java/com/example/spinner_app/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.spinner_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Spinner spin = findViewById(R.id.turtle_spinner);
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView result = findViewById(R.id.result_text);
+                result.setText("You select " + spin.getSelectedItem());
+
+                ImageView image = findViewById(R.id.turtle_image);
+                image.setImageResource(R.drawable.donatello);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
