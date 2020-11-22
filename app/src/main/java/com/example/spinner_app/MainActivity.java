d8b560c6318b4e1c3dc73a50871ba4f54db4ff40
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-11-22 12:47:22 +0400

    some changes

--- app/src/main/java/com/example/spinner_app/MainActivity.java
+++ app/src/main/java/com/example/spinner_app/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.Toast.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +24,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 TextView result = findViewById(R.id.result_text);
-                result.setText("You select " + spin.getSelectedItem());
+                String turtle = (String) spin.getSelectedItem();
+                result.setText("You select " + turtle);
 
                 ImageView image = findViewById(R.id.turtle_image);
-                image.setImageResource(R.drawable.donatello);
+//                image.setImageResource(R.drawable.raphael);
+
+                if(turtle == "Donatello") {
+                    image.setImageResource(R.drawable.donatello);
+                }
+                else if(turtle == "Raphael") {
+                    image.setImageResource(R.drawable.raphael);
+                }
+                else if (turtle == "Leonardo") {
+                    image.setImageResource(R.drawable.leonardo);
+                }
+                else if(turtle == "Turtle") {
+                    image.setImageResource(R.drawable.turtle);
+                }
+
+                Toast.makeText(parent.getContext(), "You select " + turtle, LENGTH_SHORT).show();
             }
+
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
